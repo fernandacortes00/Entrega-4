f60812dd0b7cc3553407fd81169df22f79f5406c
Sol Alzú <88586366+solalzu@users.noreply.github.com>
2022-07-24 19:26:01 -0300

    Update model3.py
    describí brevemente algunos comandos

--- model3.py
+++ model3.py
@@ -30,7 +30,12 @@ class Modelo3(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        # Estadísticas de zona
+        ######################## 
+        # Estadísticas de zona #
+        ########################
+        
+        # Calculamos la media por país
+        
         alg_params = {
             'COLUMN_PREFIX': '_',
             'INPUT': 'Campos_restantes_9e003774_b08e_4502_851c_441f73c464ad',
@@ -46,7 +51,10 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Guardar objetos vectoriales en archivo
+        ########################################## 
+        # Guardar objetos vectoriales en archivo #
+        ##########################################
+        
         alg_params = {
             'DATASOURCE_OPTIONS': '',
             'INPUT': 'Estadistica_zonal_49815408_9934_4670_97ec_d691ac312251',
@@ -61,7 +69,12 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Corregir geometrías
+        ####################### 
+        # Corregir geometrías #
+        #######################
+        
+        # Corregimos geometrías automáticamente
+        
         alg_params = {
             'INPUT': 'C:/Users/Pc__/Desktop/Herramientas computacionales/Clase 4/Replicar paper/Input/ne_10m_admin_0_countries.shp',
             'OUTPUT': parameters['Fix_geo_3']
@@ -73,7 +86,10 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Estadísticas de zona
+        ######################## 
+        # Estadísticas de zona #
+        ########################
+        
         alg_params = {
             'COLUMN_PREFIX': 'pop1800',
             'INPUT': 'Estadistica_zonal_c0e0ec63_c6c2_41ac_9286_6ca48f5c74ad',
@@ -89,7 +105,10 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Estadísticas de zona
+        ######################## 
+        # Estadísticas de zona #
+        ########################
+        
         alg_params = {
             'COLUMN_PREFIX': 'pop2000',
             'INPUT': 'Estadistica_zonal_68ed45f8_2f4b_41cd_9242_e49f94d2d3b0',
@@ -105,7 +124,12 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Quitar campo(s)
+        ################### 
+        # Quitar campo(s) #
+        ###################
+        
+        # Eliminamos columnas que no utilizamos
+        
         alg_params = {
             'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','TLC','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','MAPCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','POP_YEAR','GDP_MD','GDP_YEAR','ECONOMY','INCOME_GRP','FIPS_10','ISO_A2','ISO_A2_EH','ISO_A3_EH','ISO_N3','ISO_N3_EH','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_ISO','ADM0_DIFF','ADM0_TLC','ADM0_A3_US','ADM0_A3_FR','ADM0_A3_RU','ADM0_A3_ES','ADM0_A3_CN','ADM0_A3_TW','ADM0_A3_IN','ADM0_A3_NP','ADM0_A3_PK','ADM0_A3_DE','ADM0_A3_GB','ADM0_A3_BR','ADM0_A3_IL','ADM0_A3_PS','ADM0_A3_SA','ADM0_A3_EG','ADM0_A3_MA','ADM0_A3_PT','ADM0_A3_AR','ADM0_A3_JP','ADM0_A3_KO','ADM0_A3_VN','ADM0_A3_TR','ADM0_A3_ID','ADM0_A3_PL','ADM0_A3_GR','ADM0_A3_IT','ADM0_A3_NL','ADM0_A3_SE','ADM0_A3_BD','ADM0_A3_UA','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','LABEL_X','LABEL_Y','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FA','NAME_FR','NAME_EL','NAME_HE','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_UK','NAME_UR','NAME_VI','NAME_ZH','NAME_ZHT','FCLASS_ISO','TLC_DIFF','FCLASS_TLC','FCLASS_US','FCLASS_FR','FCLASS_RU','FCLASS_ES','FCLASS_CN','FCLASS_TW','FCLASS_IN','FCLASS_NP','FCLASS_PK','FCLASS_DE','FCLASS_GB','FCLASS_BR','FCLASS_IL','FCLASS_PS','FCLASS_SA','FCLASS_EG','FCLASS_MA','FCLASS_PT','FCLASS_AR','FCLASS_JP','FCLASS_KO','FCLASS_VN','FCLASS_TR','FCLASS_ID','FCLASS_PL','FCLASS_GR','FCLASS_IT','FCLASS_NL','FCLASS_SE','FCLASS_BD','FCLASS_UA'],
             'INPUT': 'Geometr_as_corregidas_d5a06c4d_5b7e_450f_8dc7_5d5a35a76453',
@@ -118,7 +142,10 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Estadísticas de zona
+        ########################
+        # Estadísticas de zona #
+        ########################
+        
         alg_params = {
             'COLUMN_PREFIX': 'pop1900',
             'INPUT': 'Estadistica_zonal_601740c0_3288_4581_b138_1e9c40750952',
@@ -134,7 +161,10 @@ class Modelo3(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Estadísticas de zona
+        ######################## 
+        # Estadísticas de zona #
+        ########################
+        
         alg_params = {
             'COLUMN_PREFIX': 'elevation',
             'INPUT': 'Estadistica_zonal_49815408_9934_4670_97ec_d691ac312251',
