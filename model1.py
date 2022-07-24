da647c7575595ceb6c6c09873daad69b7c33f05d
Fernanda Cortes <108239885+fernandacortes00@users.noreply.github.com>
2022-07-24 15:19:20 -0300

    Update model1.py
    Describí que hicimos con cada comando, anote el orden en que corrimos cada comando

--- model1.py
+++ model1.py
@@ -29,7 +29,12 @@ class Model1(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        # Drop field(s)
+        #################
+        # Drop field(s) # --> 6° comando
+        #################
+        
+        # Eliminamos las variables que no utilizamos 
+        
         alg_params = {
             'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length'],
             'INPUT': 'Calculated_47b93bb7_f568_4766_8d98_58c35e96073b',
@@ -42,7 +47,13 @@ class Model1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Feature filter
+        ################## 
+        # Feature filter # --> 4° comando
+        ##################
+        
+        # Nos quedamos con las observaciones de menos de 11 caracteres
+        # Eliminamos variables del estilo 'español, méxico' o 'español, españa' y nos quedamos con 'español'
+        
         alg_params = {
             'INPUT': 'Calculated_970442e9_7158_4226_b44c_129390bc50af',
             'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
@@ -54,13 +65,18 @@ class Model1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Field calculator clone
+        ########################## 
+        # Field calculator clone # --> 5° comando
+        ##########################
+        
+       # Clonamos NAME_PROP y la llamamos lnm 
+       
         alg_params = {
-            'FIELD_LENGTH': 10,
-            'FIELD_NAME': 'lnm',
+            'FIELD_LENGTH': 10, # Longitud de la variable
+            'FIELD_NAME': 'lnm', #Nombre de la variable
             'FIELD_PRECISION': 0,
             'FIELD_TYPE': 2,  # String
-            'FORMULA': '"NAME_PROP"',
+            'FORMULA': '"NAME_PROP"', # Utilizamos NAME_PROP para crear lnm
             'INPUT': 'menor_a_11_83574fee_d116_42f0_83a9_6c3da152a2cf',
             'OUTPUT': parameters['Field_calc']
         }
@@ -71,29 +87,37 @@ class Model1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Corregir geometrías
+        ####################### 
+        # Corregir geometrías # --> 1° comando
+        #######################
+        
+        # Corregimos geometrías automáticamente
+        
         alg_params = {
             'INPUT': '/Users/fernandacortes/Desktop/Herramientas/Clase4/langa/langa.shp',
-            'OUTPUT': parameters['Fiz_geo']
+            'OUTPUT': parameters['Fix_geo']
         }
         outputs['CorregirGeometras'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Fiz_geo'] = outputs['CorregirGeometras']['OUTPUT']
+        results['Fix_geo'] = outputs['CorregirGeometras']['OUTPUT']
 
         feedback.setCurrentStep(4)
         if feedback.isCanceled():
             return {}
 
-        # Agregar campo que auto-incrementa 
+        ##################################### 
+        # Agregar campo que auto-incrementa # --> 2° comando
+        #####################################
+        
         alg_params = {
             'FIELD_NAME': 'GID',
             'GROUP_FIELDS': [''],
-            'INPUT': outputs['CorregirGeometras']['OUTPUT'],
+            'INPUT': outputs['CorregirGeometras']['OUTPUT'], # Utilizamos como input la layer que creamos anteriormente
             'MODULUS': 0,
             'SORT_ASCENDING': True,
             'SORT_EXPRESSION': '',
             'SORT_NULLS_FIRST': False,
-            'START': 1,
-            'OUTPUT': parameters['Autoinc_id']
+            'START': 1, # Seleccionamos el valor en el que comienza
+            'OUTPUT': parameters['Autoinc_id'] # Nombramos la layer que obtenemos como output
         }
         outputs['AgregarCampoQueAutoincrementa'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Autoinc_id'] = outputs['AgregarCampoQueAutoincrementa']['OUTPUT']
@@ -102,13 +126,16 @@ class Model1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Calculadora de campos
+        ######################### 
+        # Calculadora de campos # --> 3° comando
+        #########################
+        
         alg_params = {
             'FIELD_LENGTH': 2,
-            'FIELD_NAME': 'length',
+            'FIELD_NAME': 'length', # Nombre de la variable
             'FIELD_PRECISION': 0,
             'FIELD_TYPE': 1,  # Integer
-            'FORMULA': 'length(NAME_PROP)',
+            'FORMULA': 'length(NAME_PROP)', # Calcula length de la variable (NAME_PROP)
             'INPUT': outputs['AgregarCampoQueAutoincrementa']['OUTPUT'],
             'OUTPUT': parameters['Length']
         }
